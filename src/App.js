6a899946fc12649d918e6c422b87b25fafcfcb9c
SagarGuajrathi <22eg105c25@anurag.edu.in>
2023-04-14 00:03:29 +0530

    Third commit!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus, faBars, faLayerGroup, faBell, faUser, faChartLine } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faBars, faLayerGroup, faBell, faUser } from '@fortawesome/free-solid-svg-icons'
 import Taskitem from './components/Taskitem'
-
+const tasks = [
+    {
+        taskName: 'Complete this project ASAP!',
+        taskTags: ['code', 'project'],
+        taskHours: '3:15',
+        taskAssignName: 'Sagar Gujarathi',
+        taskDueDate: 'Dont know',
+        isCompleted: false,
+        isToDo: false
+    },
+    {
+        taskName: 'Start new project soon!',
+        taskTags: ['code', 'project'],
+        taskHours: '-',
+        taskAssignName: 'Sagar Gujarathi',
+        taskDueDate: '17/4/2023',
+        isCompleted: false,
+        isToDo: true
+    },
+    {
+        taskName: 'Learn new tech!',
+        taskTags: ['tech', 'learn'],
+        taskHours: '-',
+        taskAssignName: 'Sagar Gujarathi',
+        taskDueDate: 'Dont know',
+        isCompleted: false,
+        isToDo: true
+    },
+    {
+        taskName: 'Post the project progress in linkedin',
+        taskTags: ['post', 'project'],
+        taskHours: '1:00',
+        taskAssignName: 'Sagar Gujarathi',
+        taskDueDate: 'Today',
+        isCompleted: false,
+        isToDo: true
+    }
+]
 function App() {
     return (
         <>
-            {/* <div className="left-container">
-                <span> <FontAwesomeIcon icon={faChartLine} />  &nbsp; DASHBOARD</span>
-                <span> <FontAwesomeIcon icon={faChartLine} />  &nbsp; DASHBOARD</span>
-                <span> <FontAwesomeIcon icon={faChartLine} />  &nbsp; DASHBOARD</span>
-                <span> <FontAwesomeIcon icon={faChartLine} />  &nbsp; DASHBOARD</span>
-                <span> <FontAwesomeIcon icon={faChartLine} />  &nbsp; DASHBOARD</span>
-                <span> <FontAwesomeIcon icon={faChartLine} />  &nbsp; DASHBOARD</span>
-                <span> <FontAwesomeIcon icon={faChartLine} />  &nbsp; DASHBOARD</span>
-
-            </div> */}
             <div className="main-layout">
                 <div className="right-container">
                     <div className="search-bar">
@@ -38,54 +65,32 @@ function App() {
                             <FontAwesomeIcon icon={faPlus} style={{ color: "#ffffff", }} />
                         </div>
                     </div>
-                    <table className="task-table">
-                        <Taskitem taskName={'#  Task name'} taskTags={'Task Tags'} taskHours={'Hours'} taskAssignName={'Task Assign name'} taskDueDate={'Due Date'} style={{ fontWeight: '600', borderRadius: '5px' }} />
-                        <tr className="empty"><td></td></tr>
-                        <Taskitem taskName={'To Do'} style={{ fontWeight: '600', fontSize: '1rem' }} />
-                        <Taskitem taskName={'goat'} isTask={true} />
-                        <Taskitem taskName={'goat'} isTask={true} />
-                        <Taskitem taskName={'goat'} isTask={true} />
-                        <tr className="empty"><td></td></tr>
-                        <Taskitem taskName={'In Progress'} style={{ fontWeight: '600', fontSize: '1rem' }} />
-                        <Taskitem taskName={'goat'} isTask={true} />
-                        <Taskitem taskName={'goat'} isTask={true} />
-                        <Taskitem taskName={'goat'} isTask={true} />
-                    </table>
-                </div>
-            </div>
-            <div className="task-add-popup">
-                <div className="search-bar">
-                    <input type="text" placeholder='Enter your task here' className='input-bar' />
-                </div>
-                <div className="search-bar">
-                    <input type="text" placeholder='Enter the task tags here' className='input-bar' />
-                </div>
-                <div className="search-bar">
-                    <input type="text" placeholder='Enter the task assign name' className='input-bar' />
-                </div>
-                <div className="task-date-popup">
-
-                    <div className="search-bar">
-                        <label htmlFor="input-hours">Hours : </label>
-                        <input type="time" className='input-bar' name='input-hours' />
-                    </div>
-                    <div className="search-bar">
-                        <label htmlFor="input-due-date">Due Date : </label>
-                        <input type="date" className='input-bar' name='input-due-date' />
+                    <div className="primary-task-container">
+                        <div className="secondary-task-container">
+                            <Taskitem taskName={'# Task name'} taskTags={'Task Tags'} taskHours={'Hours'} taskAssignName={'Task Assign name'} taskDueDate={'Due Date'} style={{ fontWeight: '600' }} />
+                        </div>
+                        <div className="secondary-task-container">
+                            <Taskitem taskName={'To Do'} style={{ fontWeight: '600' }} />
+                            {
+                                tasks.filter(task => { return task.isToDo }).map(task => {
+                                    return (
+                                        <Taskitem taskName={task.taskName} taskTags={task.taskTags} taskHours={task.taskHours} taskAssignName={task.taskAssignName} taskDueDate={task.taskDueDate} isTask={true} isCompleted={task.isCompleted} />
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className="secondary-task-container">
+                            <Taskitem taskName={'In Progress'} style={{ fontWeight: '600' }} />
+                            {
+                                tasks.filter(task => { return !task.isToDo }).map(task => {
+                                    return (
+                                        <Taskitem taskName={task.taskName} taskTags={task.taskTags} taskHours={task.taskHours} taskAssignName={task.taskAssignName} taskDueDate={task.taskDueDate} isTask={true} isCompleted={task.isCompleted} />
+                                    )
+                                })
+                            }
+                        </div>
                     </div>
                 </div>
-                <div className="task-date-popup">
-
-                    <div className="search-bar" style={{ backgroundColor: 'transparent' }}>
-                        <label htmlFor="input-due-date">Due Date : </label>
-                        <input type="radio" className='input-bar' name='input-due-date' />
-                    </div>
-                    <div className="search-bar" style={{ backgroundColor: 'transparent' }}>
-                        <label htmlFor="input-due-date">In Progress : </label>
-                        <input type="radio" className='input-bar' name='input-due-date' />
-                    </div>
-                </div>
-
             </div>
         </>
     )
--- src/components/Taskitem.js
+++ src/components/Taskitem.js
@@ -1,35 +1,91 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import { faCircleCheck, faCaretDown, faTrash, faPenToSquare, faClock, faCalendarDays, faUser } from '@fortawesome/free-solid-svg-icons'
+import '../css/Taskitem.css'
 function Taskitem(props) {
     const primary = { color: '#909395' }
     const secondary = { color: '#60B058' }
-    const [status, setStatus] = useState(primary)
-
+    const [status, setStatus] = useState(props.isCompleted)
+    const [rotation, setRotation] = useState({ rotate: '-90deg' })
+    const Colors = [{ backgroundColor: 'rgba(95, 175, 88, 0.1)', color: '#5FAF58' }, { backgroundColor: 'rgba(246, 182, 44 , 0.1)', color: '#F6B62C' }, { backgroundColor: 'rgba(247, 65, 60, 0.1)', color: '#F7413C' }, { backgroundColor: 'rgba(207, 226, 255,0.1)', color: '#CFE2FF' }, { backgroundColor: 'rgba(209, 231, 221, 0.1)', color: '#D1E7DD' }]
     return (
-        <tr className="table-item" style={props.style}>
-            <td className="task-name">
-                <span>
-                    {
-                        (props.isTask) ? <FontAwesomeIcon className='pointer' icon={faCircleCheck} style={status} onClick={() => {
-                            if (primary.color === status.color) {
-                                setStatus(secondary)
+        <div className="task-item" style={props.style}>
+            <div className="task-name">
+
+                {
+                    (props.isTask) ? <>
+                        <FontAwesomeIcon className="clickable" icon={faCaretDown} style={rotation} onClick={() => {
+                            if (rotation.rotate == '-90deg') {
+                                setRotation({ rotate: '0deg' })
                             }
                             else {
-                                setStatus(primary)
+                                setRotation({ rotate: '-90deg' })
                             }
-                        }} /> : ''
-                    }
-                </span>
-                <span>
-                    {props.taskName}
-                </span>
-            </td>
-            <td className="task-tags">{props.taskTags}</td>
-            <td className="task-hours">{props.taskHours}</td>
-            <td className="task-assign-name">{props.taskAssignName}</td>
-            <td className="task-due-date">{props.taskDueDate}</td>
-        </tr>
+                        }} />
+                        <FontAwesomeIcon className='clickable' icon={faCircleCheck} style={status ? secondary : primary} onClick={() => {
+                            if (status) {
+                                setStatus(false)
+                            }
+                            else {
+                                setStatus(true)
+                            }
+                        }} />
+                    </> : ''
+                }
+                <span style={{ textDecoration: (status) ? 'line-through' : '' }}>{props.taskName}</span>
+            </div>
+            <div className="task-tags">
+                {
+                    (typeof (props.taskTags) == 'object') ?
+                        props.taskTags.map((tag, i) => {
+                            return (
+                                <div className="tag-container" style={Colors[i]}>
+                                    {tag}
+                                </div>
+                            )
+                        })
+                        : props.taskTags
+                }
+
+            </div>
+            <div className="task-hours">{props.taskHours}</div>
+            <div className="task-assign-name">{props.taskAssignName}</div>
+            <div className="task-due-date">{props.taskDueDate}</div>
+            {
+                <div className={`view-full-detail ${(rotation.rotate == '0deg' ? 'active' : '')}`}>
+                    <div className='tag-container' style={{ fontWeight: '600' }}>{props.taskName}</div>
+                    <div className='view-full-detail-time'>
+                        <div className="tag-container" style={Colors[2]}>
+                            <FontAwesomeIcon icon={faClock} />
+                            {props.taskHours}</div>
+                        <div className='tag-container' style={Colors[3]}>
+                            <FontAwesomeIcon icon={faCalendarDays} />
+                            {props.taskDueDate}</div>
+                    </div>
+                    <div className='tag-container' style={Colors[4]}>
+                        <FontAwesomeIcon icon={faUser} />
+                        {props.taskAssignName}</div>
+                    <div className="view-full-detail-footer">
+                        <div className='view-full-detail-tag-container'>
+                            {(typeof (props.taskTags) == 'object') ?
+                                props.taskTags.map((tag, i) => {
+                                    if (i == Colors.length) {
+                                        i = i - Colors.length
+                                    }
+                                    return (
+                                        <div className="tag-container" style={Colors[i]}>{tag}</div>
+                                    )
+                                })
+                                : ''}
+                        </div>
+                        <div className='view-full-detail-edit'>
+                            <FontAwesomeIcon icon={faTrash} className='clickable' />
+                            <FontAwesomeIcon icon={faPenToSquare} className='clickable' />
+                        </div>
+                    </div>
+                </div>
+            }
+        </div >
     )
 }
 
